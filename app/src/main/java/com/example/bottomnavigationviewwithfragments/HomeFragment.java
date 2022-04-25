62c11019a9ea1b454096cf05557f9cf209030357
duongtuyen2002 <kubjnpro80@gmail.com>
2022-04-25 07:49:19 +0700

    first commit

---
+++ app/src/main/java/com/example/bottomnavigationviewwithfragments/HomeFragment.java
@@ -0,0 +1,25 @@
+package com.example.bottomnavigationviewwithfragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+    ListView LvHome;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        LvHome = view.findViewById(R.id.lvHome);
+
+        return view;
+    }
+}
